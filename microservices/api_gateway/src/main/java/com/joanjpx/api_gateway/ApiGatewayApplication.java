9b250c835c3ad252bbcc8eb8db3029094b7cf35a
Joan Perez Alvarado <joanjpx>
2024-03-13 22:48:56 -0500

    fix

---
+++ microservices/api_gateway/src/main/java/com/joanjpx/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.joanjpx.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAutoConfiguration
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
