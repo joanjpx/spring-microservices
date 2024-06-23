68546544fd725e80d5ecf43fb6b89c9ea5d02cc4
Joan Perez Alvarado <joanjpx>
2024-06-23 10:46:46 -0500

    Fixes docker-compose + application-docker.properties

--- microservices/product_service/src/main/java/com/joanjpx/product_service/ProductServiceApplication.java
+++ microservices/product_service/src/main/java/com/joanjpx/product_service/ProductServiceApplication.java
@@ -1,13 +1,25 @@
 package com.joanjpx.product_service;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class ProductServiceApplication {
+
+	private final Environment env;
+
+	public ProductServiceApplication(Environment env) {
+		this.env = env;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
+	@PostConstruct
+	public void logActiveProfiles() {
+		System.out.println("Active Profiles: " + String.join(", ", env.getActiveProfiles()));
+	}
 }
